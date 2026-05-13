c2d23decb9a37303c5db988da28b22f3f7c10601
LittleCakeLV <jeancarlos.rcj@gmail.com>
2026-05-12 23:07:12 -0300

    teste

--- simulador.py
+++ simulador.py
@@ -1,6 +1,5 @@
 import gerenciador_grafico as gg
 import tarefa as tf
-import processador as pr
 import copy
 import tkinter as tk
  
@@ -8,14 +7,15 @@ class simulador:
     ##inicialização de variaveis
     def __init__(self):
         self.tarefas = []
-        self.cpu=[]
+        self.cpu=2
         self.escalonador=''
         self.quantum=2
         self.Ggrafico = gg.gerenciador_grafico()
         # Variáveis de estado da simulação para o modo passo a passo
         self.tempo = 0
-        self.quantum_Atual = 0
+        self.quantum_Atual = self.quantum
         self.fila = [] # Fila de execução principal
+        
         self.botao_passo = None
         self.botao_executar_tudo = None
         self.botao_retroceder = None
@@ -53,7 +53,7 @@ class simulador:
         self.botao_passo.config(state="disabled")
         self.botao_executar_tudo.config(state="disabled")
         self.botao_retroceder.config(state="disabled")
-
+        
         if self.fila:
             self.passo_escalonamento()
             # Agenda a próxima execução se a simulação não tiver terminado
@@ -66,24 +66,40 @@ class simulador:
             self.finalizar_simulacao()
             return
         self.Ggrafico.desenhar_palavra(self.tempo)
-        prontas = [t for t in self.fila if t.ingresso <= self.tempo]
+        #prontas = [t for t in self.fila if t.ingresso <= self.tempo]
+        self.prontas = []
+        for fila in self.fila:
+            if fila.ingresso <= self.tempo:
+                self.prontas.append(fila)
+                #self.quantum_Atual = self.quantum # Reseta o quantum para tarefas que acabaram de chegar
         #Se houver tarefas prontas, executa a lógica de escalonamento.
-        if prontas:
-            #Reordena a fila se o quantum foi atingido
-            if self.quantum_Atual >= self.quantum:
-                if self.escalonador == "priop":
-                    prontas.sort(key=lambda t: (-t.prioridade, t.status=="Rodando", t.ingresso, t.duracao))
-                elif self.escalonador == "srtf":
-                    prontas.sort(key=lambda t: (t.duracao, t.status=="Rodando",t.ingresso))
-                self.quantum_Atual = 0 
-            # Executa as tarefas para o tempo atual e atualiza o quantum se uma tarefa terminou.
-            self.executar(self.tempo, prontas)
+        if self.prontas:
 
-        #Atualiza a fila de execução principal, removendo tarefas que já terminaram.
+            if self.quantum_Atual >= self.quantum and self.escalonador == "priop":##Organiza lista por prioridade (PRIOP)
+                   self.prontas.sort(key=lambda t: (-t.prioridade, not t.status=="Rodando", t.ingresso, t.duracao))
+                   self.quantum_Atual=0
+            elif self.escalonador == "srtf":##Organiza lista por duracao (SRTF)
+                    self.prontas.sort(key=lambda t: (t.duracao, not t.status=="Rodando",t.ingresso))
+                    self.quantum_Atual=0
+            # Executa as tarefas para o tempo atual e atualiza o quantum se uma tarefa terminou.
+            for i, iterador in enumerate(self.prontas):  
+                print(i)
+                if i < self.cpu: 
+                    iterador.status="Rodando"
+                    self.Ggrafico.desenhar_retangulo(self.tempo,iterador.id,iterador.cor)
+                    iterador.duracao-=1
+                    #print("tarefa: " + str(iterador.id) + " duracao: " + str(iterador.duracao))
+                    if iterador.duracao == 0:
+                        self.quantum_Atual=self.quantum-1       
+                else:
+                    iterador.status="Ocioso"
+                    iterador.ociosidade+=1
+                    #print("tarefa: " + str(iterador.id) + " duracao: " + str(iterador.duracao))
+                    self.Ggrafico.desenhar_retangulo(self.tempo,iterador.id,'white')   
         self.fila = [t for t in self.fila if t.duracao > 0]
-        #Avança o tempo e o quantum para o próximo passo.
         self.tempo += 1
         self.quantum_Atual += 1
+
         # Salva o estado após o passo ser concluído
         self.salvar_estado_atual()
         self.botao_retroceder.config(state="normal")
@@ -138,23 +154,9 @@ class simulador:
     #Maior prioridade é definida pela ordem da tarefa na lista de prontas,
     #ou seja, se houver 4 cpus disponíveis, as 4 primeiras tarefas da 
     #lista de prontas serão executadas nesse mesmo ciclo
-    def executar(self, tempo, prontas):
-        for i, iterador in enumerate(prontas):
-                    
-            if i < len(self.cpu): 
-                iterador.status="Rodando"
-                self.Ggrafico.desenhar_retangulo(tempo,iterador.id,iterador.cor)
-                iterador.duracao-=1
-                if iterador.duracao == 0:
-                    self.quantum_Atual=self.quantum-1
-            else:
-                iterador.status="Ocioso"
-                iterador.ociosidade+=1
-                self.Ggrafico.desenhar_retangulo(tempo,iterador.id,'white')
-            if len(prontas) < len(self.cpu):
-                self.Ggrafico.desenhar_processador(len(self.cpu)-len(prontas), tempo)
-            else:
-                self.Ggrafico.desenhar_processador(0, tempo)
+   ## def executar(self, tempo, prontas):
+
+        
                 
 
     def cria_tarefas(self):
@@ -170,8 +172,7 @@ class simulador:
                         if len(cabecalho) >= 3:
                             self.escalonador = cabecalho[0]
                             self.quantum = int(cabecalho[1])
-                            for i in range(int(cabecalho[2])):
-                                self.cpu.append(pr.processador(i))
+                            self.cpu = int(cabecalho[2])
                         continue 
                     valores = linha.split(';')
                     if len(valores) >= 5:
@@ -185,4 +186,4 @@ class simulador:
                         nova_tarefa = tf.tarefa(id_tarefa, cor, ingresso, prioridade, duracao)
                         self.tarefas.append(nova_tarefa)
         except FileNotFoundError:
-            print("Arquivo 'parametros.txt' não encontrado.")
+            print("Arquivo não encontrado.")
